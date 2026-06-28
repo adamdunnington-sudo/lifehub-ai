fe7454334ff0d57d25e862d19c92764e70ac068e
adamdunnington-sudo <adam.dunnington@hotmail.co.uk>
2026-06-28 23:21:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 
+const greeting = document.querySelector("h1");
+
+const hour = new Date().getHours();
+
+let message = "Good Evening";
+
+if (hour < 12) {
+  message = "Good Morning";
+} else if (hour < 18) {
+  message = "Good Afternoon";
+}
+
+greeting.textContent = `👋 ${message}, Adam`;
